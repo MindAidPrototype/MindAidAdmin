0eca93245a10b4a673d699fd8df079a76b475252
RobStallion <Robertefrancis18@gmail.com>
2016-05-19 12:34:20 +0100

    basic handlebars setup with refer page

--- src/server.js
+++ src/server.js
@@ -1,13 +1,26 @@
 const Hapi = require('hapi')
 const server = new Hapi.Server()
+const Path = require('path')
 const port = process.env.PORT || 4000
 const inert = require('inert')
-const plugins = [inert]
+const vision = require('vision')
+const handlebars = require('handlebars')
+
+const plugins = [inert, vision]
 
 server.connection({ port: port })
 
 server.register(plugins, error => {
   if (error) throw error
+
+  server.views({
+    engines: {html: handlebars},
+    relativeTo: __dirname + '/../',
+    path: './views',
+    layoutPath: './views/layout',
+    layout: 'default',
+    partialsPath: './views/partials/'
+  })
 
   server.route({
     method: 'GET',
@@ -24,6 +37,24 @@ server.register(plugins, error => {
       reply('stuff from server 2')
     }
   })
+
+  server.route({
+    method: 'GET',
+    path: '/refer',
+    handler: function (request, reply) {
+      reply.view('refer')
+    }
+  })
+
+  // server.route({
+  //   method: 'GET',
+  //   path: '/public/{param*}',
+  //   handler: {
+  //     directory: {
+  //       path: Path.join(__dirname,'/../public/')
+  //     }
+  //   }
+  // })
 
   server.start((err) => {
     if (err) { throw err }
