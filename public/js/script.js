c065b5b8f66f4d532ebaae18d40d1a919c6a87d3
shouston3 <shouston3@me.com>
2016-05-23 15:30:00 +0100

    buttons working for the first element on the page, cancel and save are working fine

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,3 @@
-console.log('finding the script file')
-
 const logout = () => {
   console.log('logging out')
   const xhr = new XMLHttpRequest()
@@ -14,3 +12,56 @@ const logout = () => {
 }
 
 document.getElementById('logout').addEventListener('click', logout)
+
+const editContent = () => {
+  // changing title and paragraph to text boxes with correct content
+  const title = document.getElementsByClassName('aboutParagraph')[0].children[0].innerHTML
+  const paragraph = document.getElementsByClassName('aboutParagraph')[0].children[1].innerHTML
+  document.getElementsByClassName('aboutParagraph')[0].innerHTML = ''
+  const titleText = document.createElement('input')
+  titleText.value = title
+  const paragraphText = document.createElement('input')
+  paragraphText.value = paragraph
+  document.getElementsByClassName('aboutParagraph')[0].appendChild(titleText)
+  document.getElementsByClassName('aboutParagraph')[0].appendChild(paragraphText)
+
+  // changing buttons from edit and delete to save and cancel with correct onclick event changes
+  document.getElementsByClassName('aboutEditButton')[0].innerHTML = 'save'
+  document.getElementsByClassName('aboutDeleteButton')[0].innerHTML = 'cancel'
+  document.getElementsByClassName('aboutEditButton')[0].removeEventListener('click', editContent)
+  document.getElementsByClassName('aboutEditButton')[0].addEventListener('click', saveContent)
+  document.getElementsByClassName('aboutDeleteButton')[0].removeEventListener('click', deleteContent)
+  document.getElementsByClassName('aboutDeleteButton')[0].addEventListener('click', cancelContent)
+}
+
+const deleteContent = () => {
+  console.log('deleting content')
+}
+
+const saveContent = () => {
+  const xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      location.reload()
+    }
+  }
+  xhr.open('post', '/about/save')
+  xhr.send(JSON.stringify({
+    index: 0,
+    title: document.getElementsByTagName('input')[0].value,
+    paragraph: document.getElementsByTagName('input')[1].value
+  }))
+}
+
+const cancelContent = () => {
+  console.log('cancelling content')
+  location.reload()
+}
+
+if(document.getElementsByClassName('aboutEditButton').length) {
+  document.getElementsByClassName('aboutEditButton')[0].addEventListener('click', editContent)
+}
+
+if(document.getElementsByClassName('aboutDeleteButton').length) {
+  document.getElementsByClassName('aboutDeleteButton')[0].addEventListener('click', deleteContent)
+}
