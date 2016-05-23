1a454464747c7b65cb114a8769c81402f7995bab
tas12 <tsultanah89@gmail.com>
2016-05-23 14:06:31 +0100

    start editabout

--- src/routes/about.js
+++ src/routes/about.js
@@ -1,6 +1,6 @@
 module.exports = (Cookie) => ({
   method: 'GET',
-  path: '/about',
+  path: '/about/',
   handler: (request, reply) => {
     request.state.cookie === Cookie ?
       reply.view('about', {
@@ -18,6 +18,15 @@ module.exports = (Cookie) => ({
           ]
         }
       }) :
+        reply.redirect('/login')
+  }
+},
+{
+  method: 'POST',
+  path: '/about/{type}',
+  handler: (request, reply) => {
+    request.state.cookie === Cookie ?
+     :
         reply.redirect('/login')
   }
 })
