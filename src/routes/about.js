f9a7cb7a73dc9ecba86ad1f6dc7d2cfb7ff6a392
RobStallion <Robertefrancis18@gmail.com>
2016-05-23 14:12:46 +0100

    small refactor

--- src/routes/about.js
+++ src/routes/about.js
@@ -3,30 +3,30 @@ module.exports = (Cookie) => ({
   path: '/about/',
   handler: (request, reply) => {
     request.state.cookie === Cookie ?
-      reply.view('about', {
-        about: {
-          title: 'Mind Aid',
-          body: [
-            {
-              subtitle: 'What',
-              paragraphs: ['about mindaid']
-            },
-            {
-              subtitle: 'Why',
-              paragraphs: ['why mindaid']
-            }
-          ]
-        }
-      }) :
-        reply.redirect('/login')
-  }
-},
-{
-  method: 'POST',
-  path: '/about/{type}',
-  handler: (request, reply) => {
-    request.state.cookie === Cookie ?
-     :
-        reply.redirect('/login')
+    reply.view('about', {
+      about: {
+        title: 'Mind Aid',
+        body: [
+          {
+            subtitle: 'What',
+            paragraphs: ['about mindaid']
+          },
+          {
+            subtitle: 'Why',
+            paragraphs: ['why mindaid']
+          }
+        ]
+      }
+    }) : reply.redirect('/login')
   }
 })
+// {
+//   method: 'POST',
+//   path: '/about/{type}',
+//   handler: (request, reply) => {
+//     request.state.cookie === Cookie ?
+//     reply.view('about',
+//      :
+//         reply.redirect('/login')
+//   }
+// })
