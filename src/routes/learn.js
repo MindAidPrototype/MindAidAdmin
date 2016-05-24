f3ce85b32699847f3c435a48b6458297f7f95b95
RobStallion <Robertefrancis18@gmail.com>
2016-05-24 11:56:29 +0100

    render learn

--- src/routes/learn.js
+++ src/routes/learn.js
@@ -1,9 +1,21 @@
+const MongoClient = require('mongodb').MongoClient
+const getPageData = require('../dbHelpers.js').getPageData
+require('env2')('config.env')
+
+const url = 'mongodb://localhost:27017/mindaidtest' || process.env.MONGODB_URI
+
 module.exports = (Cookie) => ({
   method: 'GET',
   path: '/learn',
   handler: (request, reply) => {
     request.state.cookie === Cookie ?
-      reply.view('learn') :
-        reply.redirect('/login')
+    MongoClient.connect(url, (err, db) => {
+      if (err) throw err
+      getPageData(db, 'learn', (res) => {
+        console.log(res)
+        reply.view('learn', {learn: res})
+      })
+
+    }) : reply.redirect('/login')
   }
 })
