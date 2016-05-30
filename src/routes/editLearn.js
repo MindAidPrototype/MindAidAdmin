6704cec3f1a7a70d5b3db691c26ce1280c16378c
tas12 <tsultanah89@gmail.com>
2016-05-30 12:36:31 +0100

    add routes for learn/save learn/savenew learn/delete

---
+++ src/routes/editLearn.js
@@ -0,0 +1,46 @@
+const MongoClient = require('mongodb').MongoClient
+const editData = require('../dbHelpers.js').editData
+const deleteData = require('../dbHelpers.js').deleteData
+const insertObjectIntoCollection = require('../dbHelpers.js').insertObjectIntoCollection
+
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mindaid'
+
+module.exports = (Cookie) => ({
+  method: 'POST',
+  path: '/learn/{type}',
+  handler: (request, reply) => {
+    if (request.state.cookie === Cookie) {
+      MongoClient.connect(url, (err, db) => {
+        if (err) throw err
+        const oldData = JSON.parse(request.payload).oldData
+        const newData = JSON.parse(request.payload).newData
+
+        switch (request.params.type) {
+        case 'save':
+          editData(db, 'learn', oldData, newData, () => {
+            db.on('close', reply('worked'))
+            db.close()
+          })
+          break
+        case 'savenew':
+          insertObjectIntoCollection(db, 'learn', newData, () => {
+            db.on('close', reply('worked'))
+            db.close()
+          })
+          break
+        case 'delete':
+          deleteData(db, 'learn', oldData, () => {
+            db.on('close', reply('worked'))
+            db.close()
+          })
+          break
+        default:
+          db.on('close', reply('wrong endpoint'))
+          db.close()
+        }
+      })
+    } else {
+      reply.redirect('/login')
+    }
+  }
+})
