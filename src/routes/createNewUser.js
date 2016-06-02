db7260aa916d6f59fdbb11ceb2d0dcbe2651f50f
shouston3 <shouston3@me.com>
2016-06-02 16:40:10 +0100

    Set up create new user route to send http request to user side to create a new user in it's db

---
+++ src/routes/createNewUser.js
@@ -0,0 +1,29 @@
+const http = require('http')
+
+module.exports = (Cookie, newUserSecret) => ({
+  method: 'post',
+  path: '/createNewUser',
+  handler: (request, reply) => {
+    console.log(request.payload, typeof request.payload, '<<<<<<<<<')
+    const user = JSON.parse(request.payload).user
+    const pass = JSON.parse(request.payload).pass
+    const payload = {
+      user,
+      pass,
+      newUserSecret
+    }
+    if(request.state.cookie === Cookie) {
+      const options = {
+        method: 'post',
+        hostname: 'localhost',
+        port: 3000,
+        path: '/createNewUser'
+      }
+      const req = http.request(options)
+      req.end(JSON.stringify(payload))
+
+    } else {
+      reply('unlucky')
+    }
+  }
+})
