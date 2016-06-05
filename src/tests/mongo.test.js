25a74aeb990957dc7d01e23d81e792b607226d6f
shouston3 <shouston3@me.com>
2016-06-05 13:33:49 +0100

    Writing different tests with finding the new changed values for the dbHelpers functions

--- src/tests/mongo.test.js
+++ src/tests/mongo.test.js
@@ -27,7 +27,6 @@ tape('checks if a collection has new inserted data in it', t => {
     db.collection('questions').insert(data, (error) => {
       if(error) throw error
       db.collection('questions').find({}).toArray((err2, res) => {
-        console.log(res)
         const lastIndex = (res.length - 1)
         const expected = 'value'
         const actual = res[lastIndex].key
@@ -42,58 +41,188 @@ tape('checks if a collection has new inserted data in it', t => {
 tape('test that data can be updated', t => {
   const oldData = {'food': 'chocolate', 'drink': 'h2o'}
   const newData = {'food': 'cake', 'drink': 'orange'}
-  var actual
   MongoClient.connect(url, (err, db) => {
-    db.collection('learn').insert(oldData, (error) => {
-      if (error) throw error
-      dbHelpers.editData(db, 'learn', oldData, newData, (res) => {
-        console.log(res)
-        actual = res.result.nModified
-        t.ok(actual === 1, 'data updated')
-        t.end()
-        db.close()
+    db.collection('learn').deleteMany({}, () => {
+      db.collection('learn').insert(oldData, (error) => {
+        if (error) throw error
+        dbHelpers.editData(db, 'learn', oldData, newData, () => {
+          db.collection('learn').find({}).toArray((err2, res) => {
+            const actual = res[0].drink
+            const expected = newData.drink
+            t.equal(actual, expected, 'updates old data to new data')
+            t.end()
+            db.close()
+          })
+        })
       })
     })
   })
 })
 
 tape('test that data can be deleted', t => {
-  var actual
+  const data = {key: 'value'}
   MongoClient.connect(url, (err, db) => {
-    const data = {key: 'value'}
-    db.collection('about').insert(data, error => {
-      if (error) throw error
-      dbHelpers.deleteData(db, 'about', data, res => {
-        actual = res.deletedCount
-        t.ok(actual === 1, 'data deleted')
-        t.end()
-        db.close()
+    db.collection('about').deleteMany({}, () => {
+      db.collection('about').insert(data, error => {
+        if (error) throw error
+        dbHelpers.deleteData(db, 'about', data, () => {
+          db.collection('about').find({}).toArray((err2, res2) => {
+            if(err2) throw err2
+            const actual = res2
+            const expected = []
+            t.deepEqual(actual, expected, 'about is cleared of data')
+            t.end()
+            db.close()
+          })
+        })
       })
     })
   })
 })
 
-tape('test updating data in an array', t => {
-  let actual
+tape('tests insertObjectIntoArray', t => {
+  const data = {
+    identifier: 'national',
+    array: [{
+      objKey1: 'objVal1'
+    }]
+  }
+  const newObject = {
+    objKey2: 'objVal2'
+  }
   MongoClient.connect(url, (err, db) => {
-    db.collection('refer').deleteOne({identifier: 'national'})
-    const data = {identifier: 'national', data: [{'number': 2}]}
-    db.collection('refer').insert(data)
-    dbHelpers.insertObjectIntoArray(db, 'refer', data.identifier, {'number': 4}, (res) => {
-      actual = res.result.nModified
-      t.ok(actual===1, 'data inserted into array')
+    db.collection('refer').deleteMany({}, () => {
+      db.collection('refer').insert(data, () => {
+        dbHelpers.insertObjectIntoArray(db, 'refer', data.identifier, newObject, () => {
+          db.collection('refer').find({}).toArray((err2, res) => {
+            if(err2) throw err2
+            const actual = res[0].array
+            const expected = [
+              {
+                objKey1: 'objVal1'
+              },
+              {
+                objKey2: 'objVal2'
+              }
+            ]
+            t.equal(actual, expected, 'inserts into an array')
+            t.end()
+            db.close()
+          })
+        })
+      })
     })
+  })
+}) 
 
-    dbHelpers.deleteObjFromArray(db, 'refer', data.identifier, {'number': 4}, (res) => {
-      actual = res.result.nModified
-      t.ok(actual===1, 'data removed from array')
+tape('tests deleteObjFromArray', t => {
+  const data = {
+    identifier: 'national',
+    array: [
+      {
+        objKey1: 'objVal1'
+      },
+      {
+        objKey2: 'objVal2'
+      }
+    ]
+  }
+  const oldObject = {
+    objKey2: 'objVal2'
+  }
+  MongoClient.connect(url, (err, db) => {
+    db.collection('refer').deleteMany({}, () => {
+      db.collection('refer').insert(data, () => {
+        dbHelpers.deleteObjFromArray(db, 'refer', data.identifier, oldObject, () => {
+          db.collection('refer').find({}).toArray((err2, res) => {
+            if(err2) throw err2
+            const actual = res[0].array
+            const expected = [
+              {
+                objKey1: 'objVal1'
+              }
+            ]
+            t.equal(actual, expected, 'deletes an object from an array')
+            t.end()
+            db.close()
+          })
+        })
+      })
     })
+  })
+}) 
 
-    dbHelpers.editObjInArray(db, 'refer', data.identifier, {'number': 2}, {'number': 5}, (res) => {
-      actual = res.result.nModified
-      t.ok(actual===1, 'data updated in array')
-      t.end()
-      db.close()
+tape('tests editObjInArray', t => {
+  const data = {
+    identifier: 'national',
+    array: [
+      {
+        objKey1: 'objVal1'
+      },
+      {
+        objKey2: 'objVal2'
+      }
+    ]
+  }
+  MongoClient.connect(url, (err, db) => {
+    if(err) throw err
+    db.collection('refer').deleteMany({}, () => {
+      db.collection('refer').insert(data, () => {
+        const oldObject = {
+          objKey2: 'objVal2'
+        }
+        const newObject = {
+          newObjKey2: 'newObjVal2'
+        }
+        dbHelpers.editObjInArray(db, 'refer', data.identifier, oldObject, newObject, () => {
+          db.collection('refer').find({}).toArray((err2, res) => {
+            if(err2) throw err2
+            const actual = res[0].array
+            const expected = [
+              {
+                objKey1: 'objVal1'
+              },
+              {
+                newObjKey2: 'newObjVal2'
+              }
+            ]
+            t.deepEqual(actual, expected, 'updates object in an array') 
+            t.end()
+            db.close()
+          })
+        })
+      })
     })
   })
 })
+
+// tape('test updating data in an array', t => {
+//   let actual
+//   MongoClient.connect(url, (err, db) => {
+//     db.collection('refer').deleteMany({}, () => {
+//       const data = {identifier: 'national', data: [{'number': 2}]}
+//       db.collection('refer').insert(data, () => {
+//       
+//       })
+//       dbHelpers.insertObjectIntoArray(db, 'refer', data.identifier, {'number': 4}, (res) => {
+//         db.collection('refer').find({}).toArray((err, res) => {
+//           if(err) throw err
+//         })
+//         actual = res.result.nModified
+//         t.ok(actual===1, 'data inserted into array')
+//       })
+// 
+//       dbHelpers.deleteObjFromArray(db, 'refer', data.identifier, {'number': 4}, (res) => {
+//         actual = res.result.nModified
+//         t.ok(actual===1, 'data removed from array')
+//       })
+// 
+//       dbHelpers.editObjInArray(db, 'refer', data.identifier, {'number': 2}, {'number': 5}, (res) => {
+//         actual = res.result.nModified
+//         t.ok(actual===1, 'data updated in array')
+//         t.end()
+//         db.close()
+//       })
+//     })
+//   })
+// })
