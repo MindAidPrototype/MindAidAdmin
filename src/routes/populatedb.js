755aa409564072716d3319d25524c1419d889e45
tas12 <tsultanah89@gmail.com>
2016-06-10 14:28:52 +0100

    handle empty collections array for when data.js is empty

--- src/routes/populatedb.js
+++ src/routes/populatedb.js
@@ -5,6 +5,7 @@ const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mindaid'
 
 const data = require('../data.js')
 const collections = Object.getOwnPropertyNames(data)
+console.log(collections)
 
 module.exports = {
   method: 'GET',
@@ -12,14 +13,18 @@ module.exports = {
   handler: function (request, reply) {
     MongoClient.connect(url, (err, db) => {
       if (err) throw err
-      dbHelpers.dropAllCollections(db, collections, (collection) => {
-        dbHelpers.insertObjectIntoCollection(db, collection, data[collection], () => {
-          if(collection === collections[collections.length -1]) {
-            db.close()
-            reply('populated b')
-          }
+      if(collections.length) {
+        dbHelpers.dropAllCollections(db, collections, (collection) => {
+          dbHelpers.insertObjectIntoCollection(db, collection, data[collection], () => {
+            if(collection === collections[collections.length -1]) {
+              db.close()
+              reply('populated b')
+            }
+          })
         })
-      })
+      } else {
+        reply('no data')
+      }
     })
   }
 }
